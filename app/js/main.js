bd949d92f3a6ab5ff08e57d2a641a7428cc2fae0
brendan8 <your_email@whatever.com>
2020-04-15 02:11:19 +0300

    Сделал слайдер и добавил звёздочки рейтига. Но пришлось подключать их CSS файл через DNS у меня почему-то не работает, хотя вроде всё подключал

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,18 @@
 $(function() {
 
     $(".rate-star").rateYo({
-        rating: 5
+        // колличество звёздочек 
+        rating: 5,
+        // размер звёздочек "ширина"
+        starWidth: "12px",
+        // заблокирует использование звёздочек
+        readOnly: true
+    });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
     });
     var mixer = mixitup('.products__inner-box');
 });
